06932a9e3b57dd7679dc12a94559bd0603b8166f
chamsedine-bouhouch <chamseddinebouhouch@gmail.com>
2021-03-01 08:28:04 +0100

    Blogs connected to User

---
+++ database/migrations/2021_03_01_065958_create_blogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->foreignId('user_id')
+            ->constrained()
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign('blogs_user_id_foreign');
+        Schema::dropIfExists('blogs');
+    }
+}
